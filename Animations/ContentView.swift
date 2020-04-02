0af0ac30402108b232101097347ff8a5b963e6e8
Ahad Islam <ahad.islam624@gmail.com>
2020-04-02 15:18:45 -0400

    animation stack

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -61,13 +61,13 @@ struct AnimationBindingView: View {
     }
 }
 
-struct ContentView: View {
+struct ExplicitAnimationView: View {
     @State private var animationAmount = 0.0
     
     var body: some View {
         Button("Tap Me") {
             withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-            self.animationAmount += 360
+                self.animationAmount += 360
             }
         }
         .padding(50)
@@ -75,6 +75,23 @@ struct ContentView: View {
         .foregroundColor(.white)
         .clipShape(Circle())
         .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+    }
+}
+
+struct ContentView: View {
+    @State private var enabled = false
+    
+    var body: some View {
+        Button("Tap Me") {
+            self.enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(.default)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+        
     }
 }
 
